4b04db230e5b71741c9eedae7c60de385e12ee86
Gustavo <gbgubento007@gmail.com>
2026-05-15 08:39:26 -0300

    atualizacoes no codigo ai

--- trabalho_ord.py
+++ trabalho_ord.py
@@ -1,44 +1,97 @@
 from __future__ import annotations
 from io import BufferedReader
+from struct import pack, unpack
+
+FORMAT_INDICE_PRIMARIO = 'HH'
+TAM_FORMAT_INDICE_PRIMARIO = 4
 
 class IndicePrimario:
 
-    valores: list[tuple[int, int]]#id e offset
+    indices: list[tuple[int, int]]#id e offset
 
     def __init__(self):
-        self.valores = []
+        self.indices = []
 
     def adiciona_elemento(self, elemento: tuple[int, int]) -> None:
         '''Adiciona elemento no indice'''
-        raise NotImplementedError
+        #insere ordenado
+        indices = self.indices
+        indices.append(elemento)
+        
+        i = len(indices) - 1
+        pos_correta = False
+        while i > 0 and not pos_correta:
+            if indices[i] < indices[i-1]:
+                indices[i], indices[i-1] = indices[i-1], indices[i]
+            else:
+                pos_correta = True
+            i -= 1
     
     def remove_elemento(self, id: str) -> None:
         '''Remove um elemento pelo id'''
         raise NotImplementedError
     
+    def cria_arquivo(self, nome_saida: str) -> None:
+        '''Cria um arquivo binário com os dados do indice'''
+        with open(nome_saida, 'wb') as saida:
+            for indice in self.indices:
+                buffer = pack(FORMAT_INDICE_PRIMARIO, *indice)
+                saida.write(buffer)
+
+    def extrai_arquivo(self, nome_entrada: str) -> None:
+        '''Extrai os dados de um arquivo binário e adiciona no indice'''
+        with open(nome_entrada, 'rb') as entrada:
+            buffer = entrada.read(TAM_FORMAT_INDICE_PRIMARIO)
+            while buffer != b'':
+                unpacked = unpack(FORMAT_INDICE_PRIMARIO, buffer)
+                self.adiciona_elemento(unpacked)
+                buffer = entrada.read(TAM_FORMAT_INDICE_PRIMARIO)
+
+    
 class IndiceSecundario:
 
-    valores: list[tuple[str, int]]#chave e valor
+    indices: list[tuple[str, int]]#chave e prim_id
     lista_invertida: ListaInvertida
 
     def __init__(self, lista_invertida: ListaInvertida):
-        self.valores = []
+        self.indices = []
         self.lista_invertida = lista_invertida
 
     def adiciona_elemento(self, chave: str, id_elemento: int) -> None:
         '''Adiciona elemento no indice'''
-        raise NotImplementedError
+        pos_chave = self.busca_chave(chave)
+        
+        if pos_chave is None:
+            #não achou a chave, insere ordenado
+            indices = self.indices
+            indices.append((chave, id_elemento))
+            
+            i = len(indices) - 1
+            pos_correta = False
+            while i > 0 and not pos_correta:
+                if indices[i] < indices[i-1]:
+                    indices[i], indices[i-1] = indices[i-1], indices[i]
+                else:
+                    pos_correta = True
+                i -= 1
+            pos_chave = i
+        
+        self.lista_invertida.adiciona_elemento()
+
     
     def remove_elemento(self, chave: str) -> None:
         '''Remove um elemento pela chave'''
         raise NotImplementedError
+    
+    def busca_chave(self, chave: str) -> int | None:
+        '''Retorna a posição da chave no indice. Se não encontrar, retorna None'''
     
 class ListaInvertida:
 
     valores: list[tuple[int, int, int]]# id, prox_gen, prox_publi
 
     def __init__(self):
-        valores = []
+        self.valores = []
 
     def adiciona_elemento(self, id_elemento: int) -> None:
         '''Adiciona elemento no indice baseado no id'''
@@ -78,39 +131,31 @@ def constroi_indices(nome_arquivo_dados: str) -> None:
             publi_regsitro = registro[4]
 
             indice_pri.adiciona_elemento([id_registro, offset])
-            indice_sec_gen.adiciona_elemento(gen_registro, id_registro)
-            indice_sec_publi.adiciona_elemento(publi_regsitro, id_registro)
+            #indice_sec_gen.adiciona_elemento(gen_registro, id_registro)
+            #indice_sec_publi.adiciona_elemento(publi_regsitro, id_registro)
 
             tam_registro_bytes = dados.read(2)
             offset += 2 + tam_registro
 
-    cria_arquivo("primairo.ind", indice_pri)
-    cria_arquivo("genero.ind", indice_sec_gen)
-    cria_arquivo("publicadora.ind", indice_sec_publi)
-    cria_arquivo("listaInvertida.lst", lista_invertida)
+    indice_pri.cria_arquivo("primario.ind")
+    # cria_arquivo("primairo.ind", indice_pri.valores)
+    # cria_arquivo("genero.ind", indice_sec_gen.valores)
+    # cria_arquivo("publicadora.ind", indice_sec_publi.valores)
+    # cria_arquivo("listaInvertida.lst", lista_invertida.valores)
 
-def cria_arquivo(nome_arquivo: str, tabela: list[tuple]) -> None:
-    '''
-    Cria um arquivo com os dados da tabela, se o arquivo já existir ele é sobrescrito
-    '''
-
-    with open(nome_arquivo, "wb") as saida:
-        
-        for registro in tabela:
-            registro_str = ""
-
-            for campo in registro:
-                registro_str += str(campo) + '|'
-
-            tamanho = len(registro_str)
-
-            saida.write(tamanho.to_bytes(byteorder="little"))
-            saida.write(registro_str.encode())
 
 
 def main() -> None:
 
     constroi_indices("games.dat")
-
+    pri = IndicePrimario()
+    pri.extrai_arquivo('primario.ind')
+    
+    with open("games.dat", 'rb') as entrada:
+        for ind in pri.indices:
+            entrada.seek(ind[1])
+            tam = int.from_bytes(entrada.read(2), 'little')
+            print(entrada.read(tam).decode().split('|'))
+    
 if __name__ == '__main__':
     main()
