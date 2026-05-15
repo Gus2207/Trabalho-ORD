b41f0992e8876510a7ed99cef72068cb49ea2324
JoseHernandesCardoso <josehhernandescardoso@gmail.com>
2026-05-15 17:27:48 -0300

    Criação dos Indices Concluida

--- trabalho_ord.py
+++ trabalho_ord.py
@@ -2,8 +2,16 @@ from __future__ import annotations
 from io import BufferedReader
 from struct import pack, unpack
 
+INDICE_DESCONHECIDO = -1
+
 FORMAT_INDICE_PRIMARIO = 'HH'
 TAM_FORMAT_INDICE_PRIMARIO = 4
+
+FORMAT_INDICE_SECUNDARIO = '60si'
+TAM_FORMAT_INDICE_SECUNDARIO = 64
+
+FORMAT_LISTA_INVERTIDA = 'Hii'
+TAM_FORMAT_LISTA_INVERTIDA = 12
 
 class IndicePrimario:
 
@@ -15,13 +23,11 @@ class IndicePrimario:
     def busca_elemento_indice(self, id: int) -> int | None:
         '''Retorna o indice de um elemento na lista, baseado em seu id.
         Retorna None se não encotrar.'''
-        i = 0
-        while i < len(self.indices) - 1:
+        for i in range(len(self.indices)) :
             if id == self.indices[i][0]:
                 return i
-            i += 1
-        
         return None
+
     
     def busca_elemento_offset(self, id: int) -> int | None:
         '''Retorna o offset de um elemento pelo seu id. Retorna None se não encontrar.'''
@@ -73,58 +79,158 @@ class IndicePrimario:
     
 class IndiceSecundario:
 
-    indices: list[tuple[str, int]]#chave e prim_id
+    indices: list[tuple[str, int]]#chave e posicao_primeiro_list_inv
     lista_invertida: ListaInvertida
+    col_prox_inv: int #Coluna de próximos elementos na lista invertida
 
-    def __init__(self, lista_invertida: ListaInvertida):
+    def __init__(self, lista_invertida: ListaInvertida, col_prox_inv: int):
         self.indices = []
         self.lista_invertida = lista_invertida
+        self.col_prox_inv = col_prox_inv
 
+    def canoniza_chave(self, chave: str) -> str:
+        '''Retorna a chave em sua forma canônica do indice secundário'''
+        return chave.replace(" ", "").upper()
+
+    def busca_chave_indice(self, chave: str) -> int | None:
+        '''Retorna a posição da chave no indice. Se não encontrar, retorna None'''
+        canonica = self.canoniza_chave(chave)
+        for i in range(len(self.indices)):
+            if self.indices[i][0] == canonica:
+                return i
+        return None
+    
+    def adiciona_chave(self, chave_canonica: str) -> int:
+        '''Adiciona uma chave que já esteja na forma canônica ao indice e retorna
+        a sua posição. O primeiro elemento do encadeamento apontado é DESCONHECIDO (-1).'''
+        indices = self.indices
+        indices.append((chave_canonica, INDICE_DESCONHECIDO))
+        
+        i = len(indices) - 1
+        pos_correta = False
+        while i > 0 and not pos_correta:
+            if indices[i] < indices[i-1]:
+                indices[i], indices[i-1] = indices[i-1], indices[i]
+                i -= 1
+            else:
+                pos_correta = True
+
+        return i
+    
     def adiciona_elemento(self, chave: str, id_elemento: int) -> None:
         '''Adiciona elemento no indice'''
-        pos_chave = self.busca_chave(chave)
-        
-        if pos_chave is None:
-            #não achou a chave, insere ordenado
-            indices = self.indices
-            indices.append((chave, id_elemento))
-            
-            i = len(indices) - 1
-            pos_correta = False
-            while i > 0 and not pos_correta:
-                if indices[i] < indices[i-1]:
-                    indices[i], indices[i-1] = indices[i-1], indices[i]
-                else:
-                    pos_correta = True
-                i -= 1
-            pos_chave = i
-        
-        self.lista_invertida.adiciona_elemento()
+        canonica = self.canoniza_chave(chave)
+        pos_chave = self.busca_chave_indice(canonica)
 
+        if pos_chave is None:
+            pos_chave = self.adiciona_chave(canonica)
+        
+        # Adiciona elemento na lista invertida
+        pos_inv = self.lista_invertida.busca_pos(id_elemento)
+        if pos_inv is None:
+            pos_inv = self.lista_invertida.adiciona_elemento(id_elemento)
+            
+        if self.indices[pos_chave][1] == INDICE_DESCONHECIDO:
+            self.indices[pos_chave] = (canonica, pos_inv)
+        
+        else:
+            pos_prim_elem = self.indices[pos_chave][1]
+            prim_elem = self.lista_invertida.busca_elem(pos_prim_elem)
+            if id_elemento < prim_elem[0]:
+                self.lista_invertida.encadeia(pos_prim_elem, pos_inv, self.col_prox_inv)
+                self.indices[pos_chave] = (canonica, pos_inv)
+            
+            else:
+                self.lista_invertida.encadeia(pos_inv, pos_prim_elem, self.col_prox_inv)
     
     def remove_elemento(self, chave: str) -> None:
         '''Remove um elemento pela chave'''
         raise NotImplementedError
     
-    def busca_chave(self, chave: str) -> int | None:
-        '''Retorna a posição da chave no indice. Se não encontrar, retorna None'''
-    
+    def cria_arquivo(self, nome_saida: str) -> None:
+        '''Cria um arquivo binário com os dados do indice'''
+        with open(nome_saida, 'wb') as saida:
+            for indice in self.indices:
+                buffer = pack(FORMAT_INDICE_SECUNDARIO, indice[0].encode(), indice[1])
+                saida.write(buffer)
+
+    def extrai_arquivo(self, nome_entrada: str) -> None:
+        '''Extrai os dados de um arquivo binário e adiciona no indice'''
+        with open(nome_entrada, 'rb') as entrada:
+            buffer = entrada.read(TAM_FORMAT_INDICE_SECUNDARIO)
+            while buffer != b'':
+                unpacked = unpack(FORMAT_INDICE_SECUNDARIO, buffer)
+                indice = (unpacked[0].decode().strip('\x00'), unpacked[1])
+                self.indices.append(indice)
+                buffer = entrada.read(TAM_FORMAT_INDICE_SECUNDARIO)
+
+
 class ListaInvertida:
 
-    valores: list[tuple[int, int, int]]# id, prox_gen, prox_publi
+    bindings: list[list[int]]# id, prox_gen, prox_publi
 
     def __init__(self):
-        self.valores = []
+        self.bindings = []
 
-    def adiciona_elemento(self, id_elemento: int) -> None:
-        '''Adiciona elemento no indice baseado no id'''
-        raise NotImplementedError
+    def busca_elem(self, posicao: int) -> list[int]:
+        '''Retorna o elemento na posição informada'''
+        return self.bindings[posicao]
+    
+    def busca_pos(self, id: int) -> int | None:
+        '''Retorna a posição do id na lista invertida.
+        Retorna None se não encontrar.'''
+        for i in range(len(self.bindings)):
+            if self.bindings[i][0] == id:
+                return i
+        return None
+
+    def adiciona_elemento(self, id_elemento: int) -> int:
+        '''Adiciona elemento no indice baseado no id e retorna sua posição'''
+        self.bindings.append([id_elemento, INDICE_DESCONHECIDO, INDICE_DESCONHECIDO])
+        return len(self.bindings) - 1
     
     def remove_elemento(self, id: int) -> None:
         '''Remove um elemento pelo id'''
         raise NotImplementedError
-#contrução de indices
+    
+    def encadeia(self, pos_novo_elem: int, pos_elem_base: int, col: int) -> None:
+        '''Adiciona o elemento ao encadeamento da coluna'''
+        novo_elem = self.bindings[pos_novo_elem]
+        elem_base = self.bindings[pos_elem_base]
+        pos_prox_elem = elem_base[col]
+        prox_elem = self.bindings[pos_prox_elem]
 
+        if pos_prox_elem is INDICE_DESCONHECIDO:
+            elem_base[col] = pos_novo_elem
+        elif novo_elem[0] < prox_elem[0]:
+            novo_elem[col] = elem_base[col]
+            elem_base[col] = pos_novo_elem
+
+        elif novo_elem[0] > prox_elem[0]:
+            if pos_prox_elem is INDICE_DESCONHECIDO:
+                prox_elem[col] = pos_novo_elem
+            else:
+                self.encadeia(pos_novo_elem, pos_prox_elem, col)
+
+    def cria_arquivo(self, nome_saida: str) -> None:
+        '''Cria um arquivo binário com os dados da lista invertida'''
+        with open(nome_saida, 'wb') as saida:
+            for bind in self.bindings:
+                buffer = pack(FORMAT_LISTA_INVERTIDA, *bind)
+                saida.write(buffer)
+
+    def extrai_arquivo(self, nome_entrada: str) -> None:
+        '''Extrai os dados de um arquivo binário e adiciona na lista invertida'''
+        with open(nome_entrada, 'rb') as entrada:
+            buffer = entrada.read(TAM_FORMAT_LISTA_INVERTIDA)
+            while buffer != b'':
+                unpacked = unpack(FORMAT_LISTA_INVERTIDA, buffer)
+                self.bindings.append(list(unpacked))
+                buffer = entrada.read(TAM_FORMAT_LISTA_INVERTIDA)
+
+
+
+#contrução de indices
 def constroi_indices(nome_arquivo_dados: str) -> None:
     '''
     Constroi baseado no arquivo de dados os arquivos de indice primario (primario.ind) indices secundarios
@@ -136,8 +242,8 @@ def constroi_indices(nome_arquivo_dados: str) -> None:
 
         indice_pri = IndicePrimario()
         lista_invertida = ListaInvertida()
-        indice_sec_gen = IndiceSecundario(lista_invertida)
-        indice_sec_publi = IndiceSecundario(lista_invertida)
+        indice_sec_gen = IndiceSecundario(lista_invertida, 1)
+        indice_sec_publi = IndiceSecundario(lista_invertida, 2)
         
         offset = 0
         tam_registro_bytes = dados.read(2)
@@ -153,32 +259,29 @@ def constroi_indices(nome_arquivo_dados: str) -> None:
             gen_registro = registro[3]
             publi_regsitro = registro[4]
 
-            indice_pri.adiciona_elemento([id_registro, offset])
-            #indice_sec_gen.adiciona_elemento(gen_registro, id_registro)
-            #indice_sec_publi.adiciona_elemento(publi_regsitro, id_registro)
+            indice_pri.adiciona_elemento((id_registro, offset))
+            indice_sec_gen.adiciona_elemento(gen_registro, id_registro)
+            indice_sec_publi.adiciona_elemento(publi_regsitro, id_registro)
 
             tam_registro_bytes = dados.read(2)
             offset += 2 + tam_registro
 
     indice_pri.cria_arquivo("primario.ind")
-    # cria_arquivo("primairo.ind", indice_pri.valores)
-    # cria_arquivo("genero.ind", indice_sec_gen.valores)
-    # cria_arquivo("publicadora.ind", indice_sec_publi.valores)
-    # cria_arquivo("listaInvertida.lst", lista_invertida.valores)
+    indice_sec_gen.cria_arquivo("genero.ind")
+    indice_sec_publi.cria_arquivo("publicadora.ind")
+    lista_invertida.cria_arquivo("listaInvertida.lst")
 
 
 
 def main() -> None:
 
     constroi_indices("games.dat")
-    pri = IndicePrimario()
-    pri.extrai_arquivo('primario.ind')
+    lst_inv = ListaInvertida()
+    lst_inv.extrai_arquivo('listaInvertida.lst')
     
     with open("games.dat", 'rb') as entrada:
-        for ind in pri.indices:
-            entrada.seek(ind[1])
-            tam = int.from_bytes(entrada.read(2), 'little')
-            print(entrada.read(tam).decode().split('|'))
+        for ind in lst_inv.bindings:
+            print(ind)
     
 if __name__ == '__main__':
     main()
